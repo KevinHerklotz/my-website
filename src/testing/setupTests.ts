98bc27ec2344e8175da18f14353078d3695effe1
Kevin Herklotz <kevin.herklotz@ricardo.ch>
2023-10-30 17:35:30 +0100

    fix failing test by mocking IntersectionObserver

--- src/testing/setupTests.ts
+++ src/testing/setupTests.ts
@@ -1,6 +1,7 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import '@testing-library/jest-dom/vitest' // adds custom matchers like `toBeInTheDocument`, works also with Vitest
 import React from 'react'
+import { vi } from 'vitest'
 
 global.React = React
 
@@ -12,3 +13,15 @@ window.matchMedia =
     addListener() {},
     removeListener() {},
   }))
+
+// needed for framer-motion
+const intersectionObserverMock = function intersectionObserver() {
+  return {
+    observe: vi.fn(),
+    disconnect: vi.fn(),
+    takeRecords: vi.fn(),
+    unobserve: vi.fn(),
+  }
+}
+
+vi.stubGlobal('IntersectionObserver', intersectionObserverMock)
