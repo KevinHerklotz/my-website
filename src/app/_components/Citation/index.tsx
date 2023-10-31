524b0bed82a2e3d890ee91ef714e81ab8fac46fa
Kevin Herklotz <kevin.herklotz@ricardo.ch>
2023-10-31 08:08:51 +0100

    animate citations

--- src/app/_components/Citation/index.tsx
+++ src/app/_components/Citation/index.tsx
@@ -1,3 +1,7 @@
+'use client'
+
+import { motion } from 'framer-motion'
+
 type CitationProps = {
   quote: string
   who: string
@@ -6,10 +10,20 @@ type CitationProps = {
 export default function Citation({ quote, who }: CitationProps) {
   return (
     <>
-      <blockquote className="inline-block w-auto max-w-3xl rounded-md border-2 border-solid border-neutral-100 border-opacity-50 bg-white bg-opacity-10 p-3 font-extralight italic">
+      <blockquote className="relative inline-block w-auto max-w-3xl overflow-hidden rounded-md border-2 border-solid border-neutral-100 border-opacity-50 bg-white bg-opacity-10 p-3 font-extralight italic">
+        <motion.div
+          className="absolute -bottom-5 -top-5 w-[10%] rotate-[20deg] bg-gradient-to-r from-transparent from-30% via-white to-70% opacity-10"
+          initial={{ left: '-10%' }}
+          whileInView={{ left: '110%' }}
+          transition={{ duration: 0.5, delay: 0.3 }}
+          viewport={{
+            once: true,
+            amount: 0.05,
+          }}
+        />
         &quot;{quote}&quot;
       </blockquote>
-      <figcaption className="mb-6 mt-0.5 text-lg sm:mb-10 sm:text-2xl">({who})</figcaption>
+      <figcaption className="-mt-1 mb-6 text-lg sm:mb-10 sm:text-2xl">({who})</figcaption>
     </>
   )
 }
